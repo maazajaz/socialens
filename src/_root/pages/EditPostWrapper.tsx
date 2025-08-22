1753da26f5ed557ef8de794feada3539e4393583
Maaz Ajaz <maazajaz1234@gmail.com>
2025-08-23 01:46:14 +0530

    Fix home feed centering and remove unused import warning

--- src/_root/pages/EditPostWrapper.tsx
+++ src/_root/pages/EditPostWrapper.tsx
@@ -2,7 +2,7 @@
 
 import PostFormNextJS from '../../components/forms/PostFormNextJS';
 import { useGetPostById } from '../../lib/react-query/queriesAndMutations';
-import ClientLayoutWrapper from '../ClientLayoutWrapper';
+
 import Loader from '../../components/shared/Loader';
 
 interface EditPostWrapperProps {
@@ -29,23 +29,21 @@ const EditPostWrapper = ({ postId }: EditPostWrapperProps) => {
   }
 
   return (
-    <ClientLayoutWrapper>
-      <div className="flex flex-1">
-        <div className="common-container">
-          <div className="flex-start gap-3 justify-start w-full max-w-5xl">
-            <img
-              src="/assets/icons/edit.svg"
-              width={36}
-              height={36}
-              alt="edit"
-              className="invert-white"
-            />
-            <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
-          </div>
-          <PostFormNextJS action="Update" post={post} />
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start gap-3 justify-start w-full max-w-5xl">
+          <img
+            src="/assets/icons/edit.svg"
+            width={36}
+            height={36}
+            alt="edit"
+            className="invert-white"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Edit Post</h2>
         </div>
+        <PostFormNextJS action="Update" post={post} />
       </div>
-    </ClientLayoutWrapper>
+    </div>
   );
 };
 
